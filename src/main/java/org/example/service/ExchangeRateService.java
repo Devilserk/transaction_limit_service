96ff6511d9828dcd3e538f1d3cb727f5650df33a
Devilserk <demonserker@gmail.com>
2023-09-25 16:09:25 +0600

    some fixes

--- src/main/java/org/example/service/ExchangeRateService.java
+++ src/main/java/org/example/service/ExchangeRateService.java
@@ -3,6 +3,7 @@ package org.example.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.ExchangeRate;
@@ -11,10 +12,14 @@ import org.example.repository.ExchangeRateRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -24,19 +29,18 @@ import java.util.List;
 @Slf4j
 public class ExchangeRateService {
     @Value("${twelvedata.api.url.base}")
-    private  String baseUrl;
+    private String baseUrl;
     @Value("${twelvedata.api.url.daily-currency-close}")
     private String dailyCurrencyUrl;
 
     private final ExchangeRateRepository exchangeRateRepository;
     private final ObjectMapper objectMapper;
+    private List<String> currencyPairs;
 
     public void updateExchangeRates() {
         WebClient webClient = WebClient.builder()
                 .baseUrl(baseUrl)
                 .build();
-
-        List<String> currencyPairs = Arrays.asList("USD/RUB");
 
         for (String currencyPair : currencyPairs) {
             String url = String.format(dailyCurrencyUrl, currencyPair);
@@ -50,6 +54,11 @@ public class ExchangeRateService {
                         .block());
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
+            }
+
+            if (!jsonNode.at("/status").asText().equals("ok")) {
+                log.error(jsonNode.toString());
+                break;
             }
 
             ExchangeRate exchangeRate = new ExchangeRate();
@@ -66,7 +75,7 @@ public class ExchangeRateService {
 
     public BigDecimal convertTransactionSumToUSD(Transaction transaction) {
         String currencyPair = "USD/" + transaction.getCurrencyShortname().getCurrencyCode();
-        return transaction.getSum().divide(getExchangeRate(currencyPair).getCloseRate(), 2, RoundingMode.HALF_DOWN);
+        return transaction.getSum().multiply(getExchangeRate(currencyPair).getCloseRate());
     }
 
     public ExchangeRate getExchangeRate(String currencyPair) {
@@ -74,8 +83,18 @@ public class ExchangeRateService {
                 .orElseThrow(() -> new IllegalArgumentException("Currency pair not found: " + currencyPair));
     }
 
-    @Scheduled(cron = "0 0 1 * * ?") // Запускать каждый день в 01:00
+    @Scheduled(cron = "0 0 1 * * ?")
     public void scheduledUpdate() {
         updateExchangeRates();
+    }
+
+    @PostConstruct
+    private void initialize() {
+        Path path = Path.of("./src/main/resources/currency/pairs.txt");
+        try {
+            currencyPairs = Files.readAllLines(path).stream().map(String::trim).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
